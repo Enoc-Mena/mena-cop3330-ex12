169573cb4369ad1d89b65bc9bd00a56f3c4a1515
Enoc-Mena <77991403+Enoc-Mena@users.noreply.github.com>
2021-06-02 15:05:03 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.simpleinterest;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double principle, interestRate, years;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the principal: ");
+        principle = scan.nextDouble();
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Enter the rate of interests: ");
+        interestRate = scan2.nextDouble();
+
+        Scanner scan3 = new Scanner(System.in);
+        System.out.print("Enter the number of years: ");
+        years = scan3.nextDouble();
+
+        double newInterestRate = interestRate / 100;
+
+        double simpleInterest = principle * (1 + (newInterestRate * years));
+
+        System.out.print("After " + years + " years at " + interestRate + "%, the investment will be worth $" + simpleInterest + ".");
+        
+    }
+}
